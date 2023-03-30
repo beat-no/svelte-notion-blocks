84a351b6ccc8b419e60b4ceb52cb31b381871417
Vegard Larsen <vegard@beat.no>
2023-03-30 15:58:15 +0200

    Fixes #1: Indentation of bullet and numbered lists.

--- src/lib/blocks/BulletListItem.svelte
+++ src/lib/blocks/BulletListItem.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
-	import type { BulletedListItemBlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
-    import RichText from "$lib/blocks/RichText.svelte";
+	import type { BulletedListItemBlockObjectResponse } from '@notionhq/client/build/src/api-endpoints';
+	import RichText from '$lib/blocks/RichText.svelte';
+	import Blocks from '$lib/blocks/Blocks.svelte';
 
-    export let block: BulletedListItemBlockObjectResponse;
+	export let block: BulletedListItemBlockObjectResponse;
 </script>
 
 <ul>
-    <li><RichText texts={block.bulleted_list_item.rich_text} /></li>
+	<li>
+		<RichText texts={block.bulleted_list_item.rich_text} />{#if block.has_children}<Blocks
+				blocks={block.children}
+			/>{/if}
+	</li>
 </ul>
