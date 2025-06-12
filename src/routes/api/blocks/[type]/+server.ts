8466619add6a5e6efec799d39be17e66023d873d
Vegard Andreas Larsen <vegard@beat.no>
2025-06-12 15:52:14 +0200

    Remove the Notion API key from the build output.

--- src/routes/api/blocks/[type]/+server.ts
+++ src/routes/api/blocks/[type]/+server.ts
@@ -2,11 +2,12 @@ import { pageFromSlug, agressiveCache } from '$lib/server';
 import { Client } from '@notionhq/client/build/src';
 import { json } from '@sveltejs/kit';
 import { BlockDatabase } from '../../../databases.server';
+import { env } from '$env/dynamic/private';
 
-const client = new Client({ auth: import.meta.env.VITE_NOTION_API_KEY });
+const client = new Client({ auth: env.VITE_NOTION_API_KEY });
 
 export async function GET({ params }) {
-    const { type } = params;
-    const response = await pageFromSlug(client, BlockDatabase, type);
-    return json(response, agressiveCache(3600));
+	const { type } = params;
+	const response = await pageFromSlug(client, BlockDatabase, type);
+	return json(response, agressiveCache(3600));
 }
